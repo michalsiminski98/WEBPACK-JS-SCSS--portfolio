def47b4d14e23661b1ea60557070479f3e7ffa1a
Michal Siminski <michal.siminski3@gmail.com>
2021-03-28 17:39:48 +0200

    add mobile menu buttons actions

--- js/menuButton.js
+++ js/menuButton.js
@@ -1,7 +1,34 @@
-const menuButton = document.querySelector('.navigation__menuText');
+const menuButtonOff = document.querySelector('.navigation__menuTextOff');
+const menuButtonOn = document.querySelector('.navigation__menuTextOn');
+const navigationMobile = document.querySelector('.navigationMobile');
+const ulItem = document.querySelectorAll('.navigationMobile__item');
+let number = 100;
 
 const showMenu = () => {
-  console.log('asd');
+    if(number>0){
+      number -= 5;
+      navigationMobile.style.transform= `translateX(${number}%)`;
+      requestAnimationFrame(showMenu);
+    }
 }
 
-menuButton.addEventListener('click', showMenu);
+const closeMenu = () => {
+    if(number<100){
+      number += 5;
+      navigationMobile.style.transform= `translateX(${number}%)`;
+      requestAnimationFrame(closeMenu);
+    }
+}
+
+const firstClick = () => {
+  requestAnimationFrame(showMenu)
+}
+const secondClick = () => {
+  requestAnimationFrame(closeMenu)
+}
+
+menuButtonOff.addEventListener('click', firstClick);
+menuButtonOn.addEventListener('click', secondClick);
+ulItem.forEach(element => {
+  element.addEventListener('click', secondClick);
+});
