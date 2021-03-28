4ea26b51173bdf434031342a10b7c2174c8b7249
Michal Siminski <michal.siminski3@gmail.com>
2021-03-28 15:21:26 +0200

    writing effect

--- js/writingText.js
+++ js/writingText.js
@@ -1,12 +1,19 @@
-let text = "Hello I'm Michal Siminski I'm Frontend Developer";
-const textArray = text.split('');
+let text = "Hello I'm Michal Siminski I'm";
+let textSecond = "Frontend Developer";
 let i = 0; 
+let j = 0;
 
 const writing = () => {
   if(i < text.length) {
   document.querySelector('.home__text').innerHTML += text[i];
   i++;
-  setTimeout(writing, 40);
-}
+setTimeout(writing, 80);
+  } else {
+      document.querySelector('.home__text').innerHTML += ' <span class="home__textDistinction"></span>';
+      document.querySelector('.home__textDistinction').innerHTML += textSecond[j];
+      j++;
+      let writingTimeout = setTimeout(writing, 80);
+      if(j === textSecond.length) clearTimeout(writingTimeout);
+  }
 }
 writing();
